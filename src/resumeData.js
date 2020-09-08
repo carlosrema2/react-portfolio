27029e1fef75b91b982f117815c23ff4178d0b3c
Calos <carlosrema2@gmail.com>
2020-09-08 10:53:01 -0400

    First display

---
+++ src/resumeData.js
@@ -0,0 +1,132 @@
+let resumeData = {
+    "imagebaseurl": "https://joshkuruvilla512.github.io/",
+    "name": "Carlos Reyes",
+    "role": "Full Stack Developer",
+    "linkedinId": "Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "",
+    "socialLinks": [
+      {
+        "name": "linkedin",
+        "url": "https://www.linkedin.com/in/joshua-kuruvilla/",
+        "className": "fa fa-linkedin"
+      },
+      {
+        "name": "github",
+        "url": "http://github.com/joshkuruvilla512",
+        "className": "fa fa-github"
+      },
+      {
+        "name": "skype",
+        "url": "http://twitter.com/JoshuaKuruvilla",
+        "className": "fa fa-twitter"
+      }
+    ],
+    "aboutme": "I am a Full Stack Developer with background experience in the Automotive Industry",
+    "address": "Austin, TX",
+    "website": "https://react-portfolio-ut.herokuapp.com/",
+    "education": [
+      {
+        "UniversityName": "University of Texas at Austin",
+        "specialization": "Full Stack Development",
+        "MonthOfPassing": "March",
+        "YearOfPassing": "2020",
+        "Achievements": "Certificate"
+      },
+      {
+        "UniversityName": "Oakland University",
+        "specialization": "Marketing",
+        "MonthOfPassing": "April",
+        "YearOfPassing": "2017",
+        "Achievements": "Bachelors"
+      }
+    ],
+    "work": [
+      {
+        "CompanyName": "Dürr Systems",
+        "specialization": "Logistics Manager",
+        "MonthOfLeaving": "July",
+        "YearOfLeaving": "2018-November 2019",
+        "Achievements": "Worked to make sure that the parts and equipment are delivered to the installation team on time and in the condition required for proper installation"
+        /* <li>
+          <p>Works to make sure that the parts and equipment are delivered to
+        the installation team on time and in the condition required for proper installation</p>
+        </li>
+        <li>
+          <p>Participated in Pre Installation Planning/Scheduling meetings to develop the logistics plan</p>
+        </li>
+        <li>
+          <p>Used SAP for tracking/identifying parts on arrival at site and properly label parts so they can be located in the installation plan
+            </p>
+        </li>
+        <li>
+      <p>Trained People in Durr’s Logistics Management procedures</p>
+        </li>
+      </ul> */
+      },
+      {
+        "CompanyName": "Nissan Automotive of North America",
+        "specialization": "Purchasing Analyst",
+        "MonthOfLeaving": "May",
+        "YearOfLeaving": "2017-June 2018",
+        "Achievements": "Analyze cost reduction data and related processes while making recommendations for improvement when possible"
+      }
+    ],
+    "skillsDescription": "",
+    "skills": [
+      {
+        "skillname": "HTML5"
+      },
+      {
+        "skillname": "CSS"
+      },
+      {
+        "skillname": "Reactjs"
+      }
+    ],
+    "portfolio": [
+      {
+        "name": "Colossal Hogs",
+        "description": "The PACK IS BACK",
+        "imgurl": "./images/ColossalHogs.png",
+        "imglink": "https://the-pack-is-back.herokuapp.com/"
+      },
+      {
+        "name": "Team Generator",
+        "description": "Generates a team",
+        "imgurl": "./images/TeamGeneratorProfile.png",
+        "imglink": "https://github.com/joshkuruvilla512/employee-summary"
+  
+      },
+      {
+        "name": "ATX Now!",
+        "description": "Finding concerts in Austin",
+        "imgurl": "./images/ATXNow.png",
+        "imglink": "https://kgeary.github.io/atxnow/"
+  
+      },
+      {
+        "name": "Trivia Quiz",
+        "description": "An Office themed Trivia Quiz",
+        "imgurl": "images/TrivaQuiz.png",
+        "imglink": "https://github.com/joshkuruvilla512/Trivia-Quiz"
+  
+      },{
+        "name": "Second Portfolio Page with Bootstrap",
+        "description": "Introduced to Bootstrap",
+        "imgurl": "./images/PortfolioBootstrap.png",
+        "imglink": "https://github.com/joshkuruvilla512/Homework2"
+  
+      },
+      {
+        "name": "First Portfolio Page",
+        "description": "Building blocks to coding",
+        "imgurl": "./images/Portfolio.png",
+        "imglink": "https://github.com/joshkuruvilla512/JoshKuruvilla.github.io"
+  
+      }
+    ]
+   
+  }
+  
+  export default resumeData
